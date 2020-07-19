9622212bd3c96ee3800e35bad9ab0e01b9485f86
sfpdecresc <sfpdecresc@gmail.com>
2020-07-19 00:18:59 -0700

    .

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+php -f compiler.php mappingPercentTAS.dsm shelf/new_game.txt shelf/load_game.txt shelf/keyboard.txt shelf/regadv.txt shelf/map.txt shelf/crawl.txt shelf/battle.txt mappingPercentTAS.txt
